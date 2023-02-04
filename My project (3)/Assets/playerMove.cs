5839d247abd3361387105ea6c087bea562bb0b79
ObiPivara <ricardo.affonso@live.com>
2023-02-04 01:54:53 -0300

    [update]

--- My project (3)/Assets/playerMove.cs
+++ My project (3)/Assets/playerMove.cs
@@ -13,8 +13,9 @@ public class playerMove : MonoBehaviour
     public int Player = 1;
     private Vector2 xVector = Vector2.zero;
     private Vector2 yVector = Vector2.zero;
-    private float ang = 0;
-
+    private int LogTerm = 0;
+    private int movimentos = 3;
+    private int movimentosRestantes = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,12 +26,14 @@ public class playerMove : MonoBehaviour
     void Update()
     {
         if(State == 0){
+            movimentosRestantes = movimentos;
             LastSpownd = Node;
             CurSpown = Instantiate(Node, Roots.transform); 
             CurSpown.transform.position  = CurSpown.transform.position+ new Vector3(0,-1,0);
             State = 1;
         }
         if(State == 1){
+            
             float horizontalInput = Input.GetAxis ("P"+Player+"_Horizontal");
             float vertialInput = 1; 
             if(horizontalInput == 1 || horizontalInput == -1){
@@ -45,16 +48,29 @@ public class playerMove : MonoBehaviour
 
             //CurSpown.transform.Rotate(new Vector3( 0,0, ang)); 
             
-            if(Input.GetKeyUp("joystick "+Player+" button 2")){
-                LastSpownd=CurSpown ;
-                CurSpown = Instantiate(Node, Roots.transform); 
-                CurSpown.transform.position  = LastSpownd.transform.GetChild(1).transform.position;
+            if(Input.GetKeyUp("joystick "+Player+" button 2") && movimentosRestantes > 0){
                 
-                CurSpown.transform.Rotate(LastSpownd.transform.GetChild(1).transform.eulerAngles); 
+                if(movimentosRestantes != 0){
+                    LastSpownd=CurSpown ;
+                    CurSpown = Instantiate(Node, Roots.transform); 
+                    CurSpown.transform.position  = LastSpownd.transform.GetChild(1).transform.position;
+                    CurSpown.transform.Rotate(LastSpownd.transform.GetChild(1).transform.eulerAngles); 
+                    movimentosRestantes = movimentosRestantes-1;
+                }
+                
+            }
+            if(movimentosRestantes ==0){
+                State = 2;
             }
             
-            
            
+        }
+        if(State == 2){
+            if(LogTerm ==0){
+                movimentos = 3;
+            }
+            movimentosRestantes = movimentos;
+            State = 1;
         }
         
     }
